dabd9dda174ff5c107a4ce58ada13573f79ad585
franchiopin <franchioping@gmail.com>
2024-05-20 21:32:41 +0100

    alot of stuff, mostly testing

---
+++ testing/util.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <libndls.h>
+
+void util_stuff();
+
+void util_stuff(){
+
+    printf("Hello World!\n");
+
+
+    wait_key_pressed();
+    return;
+}
